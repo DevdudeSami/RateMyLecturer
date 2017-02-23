58e430fac7424e0890a9ea24ed66bf006677fcde
Youssef Moawad <youssef_programming@icloud.com>
2017-02-23 15:32:24 +0000

    updated database to final spec

--- lecturer/models.py
+++ lecturer/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class University(models.Model):
     name = models.CharField(max_length=60, unique=True)
     short_name = models.CharField(max_length=30, null=True)
+    domain = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
@@ -22,6 +24,7 @@ class Lecturer(models.Model):
     title = models.CharField(max_length=10)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    date_added = models.DateTimeField(default=timezone.now)
 
     university = models.ForeignKey(University)
     department = models.ForeignKey(Department)
@@ -45,6 +48,7 @@ class Rating(models.Model):
     value = models.IntegerField(default=0)
     user = models.ForeignKey(User)
     lecturer = models.ForeignKey(Lecturer)
+    date_rated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return "Rating by " + self.user.username + " on " + self.lecturer
@@ -53,6 +57,16 @@ class Comment(models.Model):
     comment_text = models.TextField()
     user = models.ForeignKey(User)
     lecturer = models.ForeignKey(Lecturer)
+    date_commented = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return "Comment by " + self.user.username + " on " + self.lecturer
+
+class CommentScore(models.Model):
+    value = models.IntegerField()
+    user = models.ForeignKey(User)
+    comment = models.ForeignKey(Comment)
+    date_scored = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "Score on comment: \n\n" + self.comment.comment_text + "\n\n by " + self.user.username
--- log/models.py
+++ log/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from lecturer.models import University
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
-    date_joined = models.DateTimeField(default=timezone.now)
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_active = models.DateTimeField(default=timezone.now)
+
+    university = models.ForeignKey(University)
 
     def __str__(self):
         return self.user.username
-    
+
 
 def checkUserExists(username):
     return User.objects.filter(username=username).exists()
