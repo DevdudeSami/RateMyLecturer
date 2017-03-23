6f03396b5fe56ee70754d941b94f3da17a22a6bd
Youssef Moawad <youssef_programming@icloud.com>
2017-03-23 11:48:01 +0000

    turned off debug

--- RateMyLecturer/settings.py
+++ RateMyLecturer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yho40t4glolfbl##jxnm^^8u&a@k8zz8%mnm7l-i(k+lj+f=2-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['devdude.pythonanywhere.com']
 
